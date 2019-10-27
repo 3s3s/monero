19524dadd23b497b2c75f21561e0d4dbc3af4f67
woodser <woodser@protonmail.com>
2019-10-27 16:00:56 -0400

    undo formatting

--- contrib/epee/include/net/net_ssl.h
+++ contrib/epee/include/net/net_ssl.h
@@ -45,11 +45,11 @@ namespace epee
 {
 namespace net_utils
 {
-  enum class ssl_support_t: uint8_t {
-    e_ssl_support_disabled,
-    e_ssl_support_enabled,
-    e_ssl_support_autodetect,
-  };
+	enum class ssl_support_t: uint8_t {
+		e_ssl_support_disabled,
+		e_ssl_support_enabled,
+		e_ssl_support_autodetect,
+	};
 
   enum class ssl_verification_t : uint8_t
   {
