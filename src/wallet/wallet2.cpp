c5161af4ed2e483fd0da2c3d582e1511b9776a0f
woodser <woodser@protonmail.com>
2020-04-12 17:12:39 -0400

    Update src/wallet/wallet2.cpp
    Co-Authored-By: xiphon <xiphon@protonmail.com>

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -1179,7 +1179,7 @@ wallet2::wallet2(network_type nettype, uint64_t kdf_rounds, bool unattended, std
   m_light_wallet_balance(0),
   m_light_wallet_unlocked_balance(0),
   m_original_keys_available(false),
-  m_message_store(std::move(http_client_factory->create())),
+  m_message_store(http_client_factory->create()),
   m_key_device_type(hw::device::device_type::SOFTWARE),
   m_ring_history_saved(false),
   m_ringdb(),
